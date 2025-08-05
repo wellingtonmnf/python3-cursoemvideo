1a3c984ca51644a29d2d71c045e36f80b3644538
wellingtonmnf <wellingtonmnf@outlook.com>
2025-08-04 22:54:36 -0300

    Atualização | Desafio 083 | Corrigido

--- mundo3/aulas/aula 17/aula17b.py
+++ mundo3/aulas/aula 17/aula17b.py
@@ -2,7 +2,7 @@ num = [2, 5, 9, 1]
 num[2] = 3  # Diferente das tuplas, listas são estruturas mutáveis
 num.append(7) # O metodo append adiciona um elmento ao final da lista
 num.sort(reverse=True) # Ordena a lista em ordem decrescente/alfabética
-num.insert(2,2) # Caso exista um valor anterior no INDICE, ele substitui e atualiza esse VALOR
+num.insert(2,2) # Insere no INDICE 2 o VALOR 2
 num.remove(2) # Remove o PRIMEIRO VALOR ENCONTRADO passado como parâmetro
 if 4 in num:
     num.remove(4) # Se o valor não existir na lista, retorna exceção
--- mundo3/desafios/desafio080a.py
+++ mundo3/desafios/desafio080a.py
@@ -12,8 +12,9 @@ for c in range(0, 5):
                 valores.insert(pos, n)
                 print(f'Adicionado na posição {pos} da lista')
                 break
-            if n > valores[-1]:
+            if n >= valores[-1]:
                 valores.append(n)
                 print('Adicionado ao final da lista...')
+                break
 print('-=' * 30)
 print(valores)
