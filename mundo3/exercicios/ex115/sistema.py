7473ba2806a70f97d353082c71456205317f6423
wellingtonmnf <wellingtonmnf@outlook.com>
2025-08-19 19:52:17 -0300

    Atualização | Exercício 115 (Parte 3 de 3) | Concluído

--- mundo3/exercicios/ex115/sistema.py
+++ mundo3/exercicios/ex115/sistema.py
@@ -1,12 +1,24 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opção 1')
+        # Opção de listar o conteúdo de um arquivo
+        lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        # Opção de cadastrar uma nova pessoa
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabecalho('Saíndo do sistema... Até logo!')
         break
